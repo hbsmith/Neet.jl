2796dad5c6022f3a9635c5b89a82d4416b7f40d6
autodocs <autodocs>
2018-11-16 07:08:33 +0000

    build based on 45708ea

---
+++ v0.0.1/search_index.js
@@ -0,0 +1,19 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "#",
+    "page": "Neet.jl",
+    "title": "Neet.jl",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "#Neet.jl-1",
+    "page": "Neet.jl",
+    "title": "Neet.jl",
+    "category": "section",
+    "text": "Documentation for Neet.jl"
+},
+
+]}
